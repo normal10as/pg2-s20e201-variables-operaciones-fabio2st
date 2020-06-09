eebed9ccd0cd8e739bfbc974fe88e2e57cb78d30
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-09 16:56:47 -0300

    01 Costos

--- CostosCs/Program.cs
+++ CostosCs/Program.cs
@@ -6,10 +6,23 @@ namespace CostosCs
     {
         static void Main(string[] args)
         {
-            // codigo para resolver el problema del costo
-            double a;
-            a = Math.Pow(5, 2);
-            Console.WriteLine(a);
+            // Declaración y asignación de variables
+            string descripcion;
+            decimal costoUnitario;
+            int unidadesProducidas;
+            decimal factorCostos = 3.5M;
+            decimal costoFijo = 10700;
+            //Entradas
+            Console.Write("Ingrese la descripción del artículo: ");
+            descripcion = Console.ReadLine();
+            Console.Write("Ingrese el número de unidades producidas: ");
+            unidadesProducidas = Convert.ToInt32(Console.ReadLine());
+            //Procesos
+            costoUnitario = (unidadesProducidas * factorCostos + costoFijo) / unidadesProducidas;
+            //Salidas
+            Console.WriteLine("Artículo: " + descripcion);
+            Console.WriteLine("Número de unidades: " + unidadesProducidas);
+            Console.WriteLine("Costo de producción: $" + costoUnitario);
         }
     }
 }
--- CostosVb/Program.vb
+++ CostosVb/Program.vb
@@ -2,7 +2,22 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine(Console.ReadLine())
-
+        ' Declaración y asignación de variables
+        Dim descripcion As String
+        Dim costoUnitario As Decimal
+        Dim unidadesProducidas As Integer
+        Dim factorCostos As Decimal = 3.5
+        Dim costoFijo As Decimal = 10700
+        ' Entradas
+        Console.Write("Ingrese la descripción del artículo: ")
+        descripcion = Console.ReadLine()
+        Console.Write("Ingrese el número de unidades producidas: ")
+        unidadesProducidas = Console.ReadLine()
+        ' Procesos
+        costoUnitario = (unidadesProducidas * factorCostos + costoFijo) / unidadesProducidas
+        'Salidas
+        Console.WriteLine("Artículo: " & descripcion)
+        Console.WriteLine("Número de unidades: " & unidadesProducidas)
+        Console.WriteLine("Costo de producción: $" & costoUnitario)
     End Sub
 End Module
