6f616bc0181a3033f2ef8fed7fc1c61715cc7ccb
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-09 22:28:02 -0300

    Equivalencia horas

--- EquivalenciasHorasCs/Program.cs
+++ EquivalenciasHorasCs/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-
+/* 1.3. Crea un proyecto llamado “EquivalenciasHoras” 
+ * que lea una cantidad de horas e imprima su
+equivalente en minutos, segundos y días */
 namespace EquivalenciasHorasCs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ushort horas = 10;
-            byte minutos = 60;
-                //segundos;
-            Console.WriteLine("Hello World!" + horas / minutos);
+            ushort horas;
+            uint minutos;
+            uint segundos;
+            ushort dias;
+            Console.Write("Ingrese cantidad horas: ");
+            horas = Convert.ToUInt16(Console.ReadLine());
+            minutos = (uint)horas * 60;
+            segundos = minutos * 60;
+            dias = (ushort)(horas / 24);
+            Console.WriteLine("Equivale a {0} segundos" , segundos);
+            Console.WriteLine("Equivale a {0} minutos" , minutos);
+            Console.WriteLine("Equivale a {0} dias" , dias);
         }
     }
 }
--- EquivalenciasHorasVb/Program.vb
+++ EquivalenciasHorasVb/Program.vb
@@ -1,11 +1,22 @@
-Imports System
+﻿Imports System
 
+' 1.3. Crea un proyecto llamado “EquivalenciasHoras” 
+'  que lea una cantidad de horas e imprima su
+' equivalente en minutos, segundos y días 
 Module Program
-    Sub Main(args As String())
-        Dim horas As UShort = 10
-        Dim minutos As Byte = 60
-        '//segundos;
-        Console.WriteLine("Hello World!" & horas / minutos)
-
+    Sub Main(ByVal args As String())
+        Dim horas As UShort
+        Dim minutos As UInteger
+        Dim segundos As UInteger
+        Dim dias As UShort
+        Console.Write("Ingrese cantidad horas: ")
+        horas = Convert.ToUInt16(Console.ReadLine())
+        minutos = CUInt(horas) * 60
+        segundos = minutos * 60
+        dias = CUShort(horas / 24)
+        Console.WriteLine("Equivale a {0} segundos", segundos)
+        Console.WriteLine("Equivale a {0} minutos", minutos)
+        Console.WriteLine("Equivale a {0} dias", dias)
     End Sub
 End Module
+
