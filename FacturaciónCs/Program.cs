3822734aaeb572855922098c53bd9d1b03734bc7
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-10 02:53:44 -0300

    Facturación

---
+++ FacturaciónCs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/* 1.8. Crea un proyecto llamado “Facturación” en el cual 
+ * se ingresa el nombre de un artículo y su costo. 
+ * La utilidad es del 150 por ciento 
+y el impuesto es el 21 por ciento. 
+Calcular e imprimir artículo, utilidad, 
+impuesto y precio de venta. */
+namespace FacturaciónCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreArticulo;
+            decimal costo, precio, utilidad, impuesto;
+            decimal utilidadPorcentaje = 150;
+            decimal impuestoPorcentaje = 21;
+
+            Console.Write("Nombre artículo: ");
+            nombreArticulo = Console.ReadLine();
+            Console.Write("Costo: ");
+            costo = Convert.ToDecimal(Console.ReadLine());
+
+            utilidad = costo * utilidadPorcentaje / 100;
+            precio = costo + utilidad;
+            impuesto = precio * impuestoPorcentaje / 100;
+            precio += impuesto;
+
+            Console.WriteLine("Articulo: " + nombreArticulo);
+            Console.WriteLine("Costo:    " + costo);  // no estaba en el requerimiento
+            Console.WriteLine("Utilidad: " + utilidad);
+            Console.WriteLine("Impuesto: " + impuesto);
+            Console.WriteLine("Precio:   " + precio);
+        }
+    }
+}
---
+++ FacturaciónVb/Program.vb
@@ -0,0 +1,31 @@
+Imports System
+' 1.8. Crea un proyecto llamado “Facturación” en el cual 
+'  se ingresa el nombre de un artículo y su costo. 
+'  La utilidad es del 150 por ciento 
+' y el impuesto es el 21 por ciento. 
+' Calcular e imprimir artículo, utilidad, 
+' impuesto y precio de venta. 
+Module Program
+    Sub Main(args As String())
+        Dim nombreArticulo As String
+        Dim costo, precio, utilidad, impuesto As Decimal
+        Dim utilidadPorcentaje As Decimal = 150
+        Dim impuestoPorcentaje As Decimal = 21
+
+        Console.Write("Nombre artículo: ")
+        nombreArticulo = Console.ReadLine()
+        Console.Write("Costo: ")
+        costo = Convert.ToDecimal(Console.ReadLine())
+
+        utilidad = costo * utilidadPorcentaje / 100
+        precio = costo + utilidad
+        impuesto = precio * impuestoPorcentaje / 100
+        precio += impuesto
+
+        Console.WriteLine("Articulo: " & nombreArticulo)
+        Console.WriteLine("Costo:    " & costo)  ' no estaba en el requerimiento
+        Console.WriteLine("Utilidad: " & utilidad)
+        Console.WriteLine("Impuesto: " & impuesto)
+        Console.WriteLine("Precio:   " & precio)
+    End Sub
+End Module
