817de8979906a411915750515f1be80453d757d9
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-09 22:08:01 -0300

    Cotizacion Terreno

---
+++ CotizacionTerrenoVb/Program.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+
+' Crear un proyecto llamado “CotizaciónTerreno” 
+'  qué calcule e imprima el costo un terreno
+' cuadrado o rectangular, teniendo como datos el ancho 
+' y la longitud en metros y el costo del
+' metro cuadrado. 
+Module Program
+    Sub Main(args As String())
+        ' Declaración y asignación de variables
+        Dim ancho, longitud As Integer
+        Dim costoMetroCuadrado, costoTotal As Decimal
+        'Entradas
+        Console.Write("Ingrese el ancho del terreno: ")
+        ancho = Convert.ToInt32(Console.ReadLine())
+        Console.Write("Ingrese la longitud del terreno: ")
+        longitud = Convert.ToInt32(Console.ReadLine())
+        Console.Write("Ingrese el precio del metro cuadrado: ")
+        costoMetroCuadrado = Convert.ToInt32(Console.ReadLine())
+        'Procesos
+        costoTotal = ancho * longitud * costoMetroCuadrado
+        'Salidas
+        Console.WriteLine("Valor cotización: $" & costoTotal)
+    End Sub
+End Module
---
+++ CotizaciónTerrenoCs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/* Crear un proyecto llamado “CotizaciónTerreno” 
+ * qué calcule e imprima el costo un terreno
+cuadrado o rectangular, teniendo como datos el ancho 
+y la longitud en metros y el costo del
+metro cuadrado. */
+namespace CotizaciónTerrenoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración y asignación de variables
+            int ancho, longitud;
+            decimal costoMetroCuadrado, costoTotal;
+            //Entradas
+            Console.Write("Ingrese el ancho del terreno: ");
+            ancho = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Ingrese la longitud del terreno: ");
+            longitud = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el precio del metro cuadrado: ");
+            costoMetroCuadrado = Convert.ToInt32(Console.ReadLine());
+            //Procesos
+            costoTotal = ancho * longitud * costoMetroCuadrado;
+            //Salidas
+            Console.WriteLine("Valor cotización: $" + costoTotal);
+        }
+    }
+}
