9e11678153dcb6bf3a8c610e49b915cf8caeb4ab
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-10 03:32:14 -0300

    Área Circulo

---
+++ ÁreaCirculoCs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/* 1.9. Crea un proyecto llamado “ÁreaCirculo” 
+ * donde se ingresa el radio del círculo imprime el
+área. 𝐴𝑟𝑒𝑎 = 𝜋𝑟2 */
+namespace ÁreaCirculoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radio, area;
+
+            Console.Write("Ingrese el radio dé un círculo: ");
+            radio = Convert.ToSingle(Console.ReadLine());
+
+            area = (Single)(Math.PI * Math.Pow(radio, 2));
+
+            Console.WriteLine("area = Pi.r^2 => {0}.{1}^2 = {2}",Math.PI, radio, area);
+        }
+    }
+}
---
+++ ÁreaCirculoVb/Program.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+' 1.9. Crea un proyecto llamado “ÁreaCirculo” 
+'  donde se ingresa el radio del círculo imprime el
+' área. 𝐴𝑟𝑒𝑎 = 𝜋𝑟2 
+Module Program
+    Sub Main(args As String())
+        Dim radio, area As Single
+
+        Console.Write("Ingrese el radio dé un círculo: ")
+
+        radio = Convert.ToSingle(Console.ReadLine())
+
+        area = CSng(Math.PI * Math.Pow(radio, 2))
+
+        Console.WriteLine("area = Pi.r^2 => {0}.{1}^2 = {2}", Math.PI, radio, area)
+    End Sub
+End Module
