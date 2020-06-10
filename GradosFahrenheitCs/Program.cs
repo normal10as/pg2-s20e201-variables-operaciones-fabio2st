13a56f66e2eef51d1e00591da6803078688eded1
Fabio Dos Santos <fabio2st@yahoo.com>
2020-06-10 01:04:32 -0300

    Grados Fahrenheit

---
+++ GradosFahrenheitCs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/* 1.6. Crea un proyecto llamado “GradosFahrenheit” 
+ * donde se ingresa la temperatura en Celsius y
+se muestra su equivalente en fahrenheit. 
+Investigar la fórmula necesaria para realizar el
+cálculo. */
+namespace GradosFahrenheitCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius, fahrenheit;
+            Console.Write("Ingrese temperatura en Celsius: ");
+            celsius = Convert.ToSingle(Console.ReadLine());
+            fahrenheit = (float)(celsius * 1.8 + 32);
+            Console.WriteLine("Temperatura en fahrenheit: " + fahrenheit);
+        }
+    }
+}
---
+++ GradosFahrenheitVb/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+' 1.6. Crea un proyecto llamado “GradosFahrenheit” 
+'  donde se ingresa la temperatura en Celsius y
+' se muestra su equivalente en fahrenheit. 
+' Investigar la fórmula necesaria para realizar el
+' cálculo. 
+Module Program
+    Sub Main(args As String())
+        Dim celsius, fahrenheit As Single
+        Console.Write("Ingrese temperatura en Celsius: ")
+        celsius = Convert.ToSingle(Console.ReadLine())
+        fahrenheit = CSng(celsius * 1.8 + 32)
+        Console.WriteLine("Temperatura en fahrenheit: " & fahrenheit)
+    End Sub
+End Module
